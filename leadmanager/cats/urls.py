e046a0ddf6010d8d13546f4645f3861ae03ceed5
Wojciech Dybikowski <wojciech.dybikowski@profil-software.com>
2020-05-02 22:57:53 +0200

    add pagination

--- leadmanager/cats/urls.py
+++ leadmanager/cats/urls.py
@@ -1,7 +1,12 @@
 from rest_framework import routers
 from .api import CatViewSet
+from django.urls import path, include
+from .views import CatsList
 
 router = routers.DefaultRouter()
 router.register('api/cats', CatViewSet, 'cats')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/cats/pagination', CatsList),
+    path('', include(router.urls))
+]
--- leadmanager/cats/views.py
+++ leadmanager/cats/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import datetime
+from .models import Cat
+from .serializers import CatSerializer
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 # Create your views here.
+
+def CatsList(request, *args, **kwargs):
+    per_page = request.GET.get('per_page')
+
+    queryset = Cat.objects.all().order_by('id')
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    paginator = Paginator(queryset, per_page or 5)
+    serializer_class = CatSerializer
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+
+    serializer = serializer_class(queryset, many=True)
+    return JsonResponse(serializer.data, safe=False)
